687bca22ff1017448745282d4121d3294216f26a
Lautaro Arino <Lautaro.Arino@hiq.se>
2018-04-03 14:15:02 +0200

    Unity project och importerade filer från tidigare projekt

---
+++ Assets/Code/ItemBase.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemBase  {
+
+    public string Name { get; set; }
+
+    int maxAmount = 100;
+    public int MaxAmount
+    {
+        get
+        {
+            return maxAmount;
+        }
+    }
+
+    int amount = 23;
+    public int Amount
+    {
+        get
+        {
+            return Amount;
+        }
+
+        set
+        {
+            Amount = value;
+        }
+    }
+
+    internal string spriteDirectoryPath = "InventoryIcons/";
+    string spriteName;
+    public string SpriteName
+    {
+        get
+        {
+            return spriteName;
+        }
+
+        set
+        {
+            spriteName = value;
+        }
+    }
+    
+    public string GetIconSpriteFilePath()
+    {
+        return spriteDirectoryPath + spriteName;
+    }
+}
---
+++ Assets/Code/OldStuff/ItemContainer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// A container where a stack of items is kept
+/// </summary>
+public class ItemContainer : MonoBehaviour
+{
+
+    [SerializeField]
+    private ItemBase stack;
+
+    public ItemBase Stack
+    {
+        get
+        {
+            return stack;
+        }
+
+        set
+        {
+            print("New stack set");
+            stack = value;
+        }
+    }
+
+
+    private void Update()
+    {
+ 
+    }
+
+    /* public void MergeSlots(ItemContainer mergeStack)
+     {
+         if (mergeStack.Stack.name == mergeStack.Stack.name)
+         {
+             Stack.DoTransaction(mergeStack.Stack);
+         }
+         else
+         {
+             var tempStash = Stack;
+             Stack = mergeStack.Stack;
+             mergeStack.Stack = tempStash;
+         }
+     } */
+}
